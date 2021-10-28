e983624e9d60596aa3a5032ae400bcc4e0d79609
popsoft01 <popoolatunde52@gmail.com>
2021-10-28 11:14:04 +0100

    estudy

---
+++ src/main/java/com/estudy/estudy/data/repository/Instructor.java
@@ -0,0 +1,7 @@
+package com.estudy.estudy.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Instructor extends JpaRepository<Instructor, Long> {
+
+}
---
+++ src/main/java/com/estudy/estudy/data/repository/LearningPartyRepository.java
@@ -0,0 +1,7 @@
+package com.estudy.estudy.data.repository;
+
+import com.estudy.estudy.data.model.LearningParty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LearningPartyRepository extends JpaRepository<LearningParty, Long> {
+}
